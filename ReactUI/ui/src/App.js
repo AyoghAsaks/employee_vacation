ee597b70a33941f1dc2e0dbbfe5e6c7caf2ab64c
AyoghAsaks <ayoghasaku@gmail.com>
2023-08-05 16:24:44 -0400

    worked on react navbar

--- ReactUI/ui/src/App.js
+++ ReactUI/ui/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<Signup/>} />
+        <Route path='/' element={<Layout/>} /> 
+        {/* <Route path='/login' element={<Login/>} /> */}
+        {/* <Route path='/signup' element={<Signup/>} /> */}
       </Routes>
     </BrowserRouter>
   )
---
+++ ReactUI/ui/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar expand="lg" bg='dark' variant='dark' collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">AYOGH</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/home">Home</Nav.Link>
+                            <Nav.Link href="/applyforvacation">Apply for Vacation</Nav.Link>
+                            <Nav.Link href="/myvacation">My Vacation</Nav.Link>
+                            <NavDropdown title="Vacation Management" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Types of Vacations</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Vacation Allocations</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Vacation Requests</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Employee Management</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header;
